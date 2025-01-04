bf92fdbf02c7cd92f43ffd3053d051824c4df834
ywcheong <tencise@gmail.com>
2025-01-04 19:43:50 +0900

    Fix: 경고 제거 단순수정

--- src/main/java/com/ywcheong/short4/data/dto/PublishResponseDTO.java
+++ src/main/java/com/ywcheong/short4/data/dto/PublishResponseDTO.java
@@ -1,8 +1,10 @@
 package com.ywcheong.short4.data.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 @AllArgsConstructor
+@Getter
 public class PublishResponseDTO {
     String manageSecret;
 }
