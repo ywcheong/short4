e54fe832760cb2c6e1aadba6e39077808b654f28
ywcheong <tencise@gmail.com>
2025-01-04 19:42:33 +0900

    Feat: DefaultShortURLRepository 구현

--- src/main/java/com/ywcheong/short4/repository/DefaultShortURLRepository.java
+++ src/main/java/com/ywcheong/short4/repository/DefaultShortURLRepository.java
@@ -2,6 +2,7 @@ package com.ywcheong.short4.repository;
 
 import com.mongodb.client.result.UpdateResult;
 import com.ywcheong.short4.data.entity.ShortURL;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
@@ -10,6 +11,7 @@ import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@Slf4j
 public class DefaultShortURLRepository implements ShortURLRepository {
     private final MongoTemplate mongoTemplate;
 
@@ -19,28 +21,52 @@ public class DefaultShortURLRepository implements ShortURLRepository {
     }
 
     @Override
-    public boolean attemptReserve(String shortUrl) {
+    public boolean attemptReserve(String token) {
         // 1. shortURL이 DB에 존재 -> 아무것도 하지 않고 false를 반환한다.
         // 2. shortURL이 DB에 부재 -> {shortURL} 삽입 후 true를 반환한다.
 
         Query queryStatement = new Query();
-        queryStatement.addCriteria(Criteria.where("shortURL").is(shortUrl));
+        queryStatement.addCriteria(Criteria.where("token").is(token));
 
         Update updateStatement = new Update();
-        updateStatement.setOnInsert("shortURL", shortUrl);
+        updateStatement.setOnInsert("shortURL", token);
 
-        UpdateResult result = mongoTemplate.upsert(
-                queryStatement,
-                updateStatement,
-                ShortURL.class
-        );
+        UpdateResult result = mongoTemplate.upsert(queryStatement, updateStatement, ShortURL.class);
 
+        if (!result.wasAcknowledged()) {
+            log.error("reserve request [{}] not acknowledged", token);
+            return false;
+        }
+
+        log.info("reserve request [{}] resulted in [{}]", token, result);
         return result.getModifiedCount() == 1;
     }
 
     @Override
     public boolean publish(ShortURL shortURL) {
-        return false;
+        Query queryStatement = new Query();
+        queryStatement.addCriteria(Criteria.where("token").is(shortURL.getToken()));
+
+        Update updateDefinition = new Update();
+        updateDefinition.set("token", shortURL.getToken()).set("originalURL", shortURL.getOriginalURL()).set("expireAfterSeconds", shortURL.getExpireAfterSeconds()).set("expireAfterVisits", shortURL.getExpireAfterVisits()).set("isActivated", shortURL.getIsActivated()).set("isForcefullyDowned", shortURL.getIsForcefullyDowned()).currentTimestamp("createdAt");
+
+        if (shortURL.getAccessSecretHash() != null) {
+            updateDefinition.set("accessSecretHash", shortURL.getAccessSecretHash());
+        }
+
+        if (shortURL.getManageSecretHash() != null) {
+            updateDefinition.set("manageSecretHash", shortURL.getManageSecretHash());
+        }
+
+        UpdateResult result = mongoTemplate.updateFirst(queryStatement, updateDefinition, ShortURL.class);
+
+        if (!result.wasAcknowledged()) {
+            log.error("publish request [{}] not acknowledged", shortURL);
+            return false;
+        }
+
+        log.info("publish request [{}] resulted in [{}]", shortURL, result);
+        return result.getModifiedCount() == 1;
     }
 
     @Override
