c926dd4f9c96089a944ac0972f4fcd26120d642d
ywcheong <tencise@gmail.com>
2025-01-08 18:03:31 +0900

    Feat: HTTPKindURLValidator 로깅 추가

--- src/main/java/com/ywcheong/short4/validator/HTTPKindURLValidator.java
+++ src/main/java/com/ywcheong/short4/validator/HTTPKindURLValidator.java
@@ -1,7 +1,9 @@
 package com.ywcheong.short4.validator;
 
+import jakarta.annotation.PostConstruct;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -10,16 +12,24 @@ import java.net.URL;
 import java.util.List;
 
 @Component
+@Slf4j
 public class HTTPKindURLValidator implements ConstraintValidator<HTTPKindURL, String> {
     @Value("${short4.server.allowed-protocols}")
     private List<String> allowedProtocols;
 
+    @PostConstruct
+    public void logAllowedProtocols() {
+        log.info("HTTP-kind URL Validator :: allowed protocol list :: [{}]", allowedProtocols);
+    }
+
     @Override
     public boolean isValid(String givenURL, ConstraintValidatorContext constraintValidatorContext) {
         try {
-            String protocol = new URL(givenURL).getProtocol();
+            String protocol = new URL(givenURL).getProtocol().toLowerCase();
+            log.debug("HTTP-kind URL Validator :: protocol given :: protocol [{}] isvalid [{}]", protocol, allowedProtocols.contains(protocol));
             return allowedProtocols.contains(protocol);
         } catch (MalformedURLException e) {
+            log.debug("HTTP-kind URL Validator :: MalformedURLException :: error [{}]", e.toString());
             return false;
         }
     }
